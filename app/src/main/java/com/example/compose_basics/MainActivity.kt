5f2e2321b1c9a948a2ce3a8d815536f0d82845cd
Amit Krishna <akrish@Amits-MacBook-Pro.local>
2022-02-20 19:28:14 +0530

    Add Images and modifiers

--- app/src/main/java/com/example/compose_basics/MainActivity.kt
+++ app/src/main/java/com/example/compose_basics/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.compose_basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose_basics.ui.theme.ComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +23,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeBasicsTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
                     Greeting(Message("Amit", "I am happy"))
                 }
             }
@@ -33,8 +41,22 @@ data class Message(
 
 @Composable
 fun Greeting(message: Message) {
-    Text(text = message.author)
-    Text(text = message.body)
+    Row(Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Contact Profile Picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+        
+        Spacer(modifier = Modifier.width(20.dp))
+
+        Column(modifier = Modifier.width(140.dp)) {
+            Text(text = message.author)
+            Text(text = message.body)
+        }
+    }
 }
 
 @Preview(showBackground = true)
